e47f1143d653586278e13b37f13e6392c1534b2a
Fazle-Rabbi-Rounok <frrounok2021@gmail.com>
2024-10-29 18:35:01 +0600

    ff

--- 01_first_pattern.cpp
+++ 01_first_pattern.cpp
@@ -3,4 +3,27 @@ using namespace std;
 
 int main()
 {
+    int fl = 0;
+    string s1 = "a cat dances";
+    string s2 = "esd";
+    int siz = s1.size() - s2.size() + 1;
+    for (int i = 0; i < siz; i++)
+        if (s1[i] == s2[0])
+        {
+            int k = i, f = 1;
+            for (int j = 0; j < s2.size(); j++, k++)
+                if (s1[k] != s2[j])
+                {
+                    f = 0;
+                    break;
+                }
+            if (f)
+            {
+                fl = 1;
+                cout << "match found\n";
+                break;
+            }
+        }
+        if(!fl)
+            cout << "no match found\n";
 }
